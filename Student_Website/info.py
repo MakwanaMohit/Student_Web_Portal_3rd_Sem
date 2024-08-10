0770868bd69a6ac04c2ca1bc5b85bf52630974a9
MakawanaMohit <makavanamohi777@gmail.com>
2024-08-10 19:23:55 +0530

    update info.py

--- Student_Website/info.py
+++ Student_Website/info.py
@@ -3,3 +3,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'your email'
 EMAIL_HOST_PASSWORD = 'your password'
 EMAIL_PORT = 587
+# hello
--- manage.py
+++ manage.py
@@ -20,3 +20,5 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+print('Mohit is good programmer')
